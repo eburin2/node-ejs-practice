724a5b15e6d8eaef27bd1e5b6094c25ad9720315
Elliot Burin <eburin2@gmail.com>
2019-03-19 13:31:59 -0700

    complete exercise

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const users = [];
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(bodyParser({extended: false}));
+
+
+app.get('/', (req, res, next) => {
+  res.render('index', { pageTitle: 'Add User' });
+});
+
+app.get('/users', (req, res, next) => {
+  res.render('users', { pageTitle: 'Users', users: users, hasUsers: 'users.length > 0' });
+});
+
+app.post('/add-user', (req, res, next) => {
+  users.push({ name: req.body.username});
+  res.redirect('/users');
+});
+
+app.listen(3000);
